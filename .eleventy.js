2591e74ce2fa9833349890491eea783e5e61ac45
Ariel Barmat <140773+abarmat@users.noreply.github.com>
2024-09-10 13:45:34 -0300

    Revamp site

---
+++ .eleventy.js
@@ -0,0 +1,57 @@
+const htmlmin = require("html-minifier");
+
+module.exports = function (eleventyConfig) {
+  // Date filter
+  eleventyConfig.addFilter("dateFilter", function (date) {
+    return new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  });
+
+  // Add a limit filter
+  eleventyConfig.addFilter("limit", function (array, limit) {
+    return array.slice(0, limit);
+  });
+
+  // Add a custom filter to create post snippets
+  eleventyConfig.addFilter("snippet", function (content, maxLength = 200) {
+    // Trim the snippet to the maximum length
+    if (content.length > maxLength) {
+      content = content.substring(0, maxLength) + "...";
+    }
+
+    return content;
+  });
+
+  // Minify HTML output
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith(".html")) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  // Pass through copy for CSS and assets
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Blog post collection
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByTag("post").reverse();
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes",
+    },
+  };
+};
